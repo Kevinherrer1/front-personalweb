d92242f4ba2996339c834e854d241d43dd1ce546
Kevinherrer1 <kevinherrerak14@gmail.com>
2025-06-08 16:05:03 -0400

    change of route app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [mensaje, setMensaje] = useState("");
 
   useEffect(() => {
-  fetch(`${import.meta.env.VITE_API_URL}/hola`)
+  fetch(`${import.meta.env.VITE_API_URL}/api/hola`)
     .then((response) => response.json())
     .then((data) => setMensaje(data.mensaje))
     .catch((error) => console.error("Error al hacer fetch:", error));
