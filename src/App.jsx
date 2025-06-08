8dc0443b28e78217ae4f92e57125d41495973c73
Kevinherrer1 <kevinherrerak14@gmail.com>
2025-06-08 15:07:11 -0400

    configuration app.jsx router

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,16 @@ function App() {
   const [mensaje, setMensaje] = useState("");
 
   useEffect(() => {
-    const API_URL = import.meta.env.VITE_API_URL;
+  fetch(`${import.meta.env.VITE_API_URL}/hola`)
+    .then((response) => response.json())
+    .then((data) => setMensaje(data.mensaje))
+    .catch((error) => console.error("Error al hacer fetch:", error));
+}, []);
 
-    fetch(`${API_URL}/hola`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMensaje(data.mensaje);
-      })
-      .catch((error) => {
-        console.error("Error al hacer fetch:", error);
-      });
-  }, []);
 
   return (
     <div>
-      <h1>Mensaje desde la API:</h1>
+      <h1>Mensaje desde la API en render:</h1>
       <p>{mensaje}</p>
     </div>
   );
